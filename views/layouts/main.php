a141f5b3630a12b865ad0bb8171d9355444644c1
rpm-16 <rpm-16@mail.ru>
2021-03-26 02:21:42 +0300

    text about changed

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -19,6 +19,8 @@ AppAsset::register($this);
     <meta charset="<?= Yii::$app->charset ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="keywords" content="<?= Html::encode($this->keywords) ?>">
+    <meta name="description" content="<?= Html::encode($this->description) ?>">
     <?php $this->registerCsrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
